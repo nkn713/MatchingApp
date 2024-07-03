dfb06603bbec8c9105686f31ef2fb8bd21f8881d
Tsuoi Morioka <al22099@shibaura-it.ac.jp>
2024-07-03 12:10:52 +0900

    change app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ app.register_blueprint(T_take_subject_bp, url_prefix='/take_subject')
 app.register_blueprint(admin_bp, url_prefix='/admin')
 app.register_blueprint(S_profile_input_bp, url_prefix='/S_profile_input')
 app.register_blueprint(T_profile_input_bp, url_prefix='/T_profile_input')
-app.register_blueprint(review_bp, url_prefix='/review')
+#app.register_blueprint(review_bp, url_prefix='/review')
 app.register_blueprint(T_take_attend_bp, url_prefix='/T_take_attend')
 app.register_blueprint(S_take_attend_bp, url_prefix='S_take_attend')
 if __name__ == '__main__':
