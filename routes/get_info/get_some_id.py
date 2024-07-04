407feb150d04496a0be3e5d4bde66451e4556f0b
Tsuoi Morioka <al22099@shibaura-it.ac.jp>
2024-07-04 10:47:03 +0900

    test

--- routes/get_info/get_some_id.py
+++ routes/get_info/get_some_id.py
@@ -7,7 +7,7 @@ def get_profile_id(login_id):
         host='your_host',
         user='your_user',
         password='your_password',
-        database='your_database'
+        database='your_database'#sssssss
     )
     cursor = conn.cursor()
 
