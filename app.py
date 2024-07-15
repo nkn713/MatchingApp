5ed8f8b916d2dfc1595cffdc8ab79f33cfa3a290
tsuoimorioka <al22099@shibaura-it.ac.jp>
2024-07-16 03:58:10 +0900

    ローカル動作用にapp.pyを変更

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ app.register_blueprint(matching_bp, url_prefix='/matching')
 app.register_blueprint(take_subject_bp, url_prefix='/S_take_subject') 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=50802)
+    app.run(debug=True)
