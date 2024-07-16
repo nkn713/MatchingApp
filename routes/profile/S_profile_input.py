088216038f3f02539374361146b6204e9e9baf66
Ito Shusei <al22020@shibaura-it.ac.jp>
2024-07-16 15:46:37 +0900

    Sprofileinput test

--- routes/profile/S_profile_input.py
+++ routes/profile/S_profile_input.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 #import mysql.connector
 from flask_mysqldb import MySQL
+from flask import current_app
 
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+#app = Flask(__name__)
+#app.secret_key = 'your_secret_key'
 
 #def get_db_connection():
 #    connection = mysql.connector.connect(
